93b35d567c0fa267715c11f9a813c1e14ac856f5
sahil-193-UI <sahil.dutt@tmotions.com>
2026-08-06 14:55:12 +0530

    Minor changes in env file

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const rawBaseUrl = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+const baseURL = rawBaseUrl.replace(/\/+$/, "");
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api/",
+  baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
